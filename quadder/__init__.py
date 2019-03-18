ce21e1283e97df835a4e0f124daa3c896a704c79
Jake Lishman <jakelishman@gmail.com>
2019-03-18 12:04:45 +0000

    Expose underlying modules
    The exporting of the functions was hiding the underlying modules, which
    made it much harder to access a particular integration rule.  The
    modules can easily be exported with a prefixed underscore, which allows
    the functions to still have matching names and makes it clear what's
    going on.

--- quadder/__init__.py
+++ quadder/__init__.py
@@ -1,5 +1,6 @@
 from .rule import QuadratureRule
 QuadratureRule.__module__ = __name__
 
-from . import plane_r2, spherical_surface
+from . import plane_r2 as _plane_r2
+from . import spherical_surface as _spherical_surface
 from .plane_r2 import plane_r2
